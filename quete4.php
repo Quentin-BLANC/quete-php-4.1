69a9d0d979a8c5159ed9e38c3f73503e4314049c
Quentin-BLANC <quentinblanc587@gmail.com>
2020-09-30 01:42:25 +0200

    quete 4.1 php

---
+++ quete4.php
@@ -0,0 +1,14 @@
+<?php
+
+$movies = [
+  "Indiana Jones et les Aventuriers de l'arche perdue" => '1981' ,
+  "Indiana Jones et le Temple maudit" => '1984' ,
+  "Indiana Jones et la Dernière Croisade" => '1989' ,
+];
+
+foreach ($movies as $movie => $date) {
+  echo $date . "-" . $movie;
+  echo PHP_EOL;
+};
+
+sort($movies);
